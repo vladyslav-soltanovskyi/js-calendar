0e932fe3527757cccd0ade0472721139eba31378
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-10 19:58:52 +0100

    added scripts for modal

--- scripts/common/modal.js
+++ scripts/common/modal.js
@@ -1,6 +1,21 @@
 const modalElem = document.querySelector('.modal');
 const modalContentElem = document.querySelector('.modal__content');
+const createEventCloseBtn = document.querySelector('.create-event__close-btn');
 
 // опишите ф-ции openModal и closeModal
 // модальное окно работает похожим на попап образом
 // отличие в том, что попап отображается в месте клика, а модальное окно - по центру экрана
+export const openModal = () => {
+  modalElem.classList.remove('hidden');
+}
+export const closeModal = () => {
+  modalElem.classList.add('hidden');
+}
+
+function onClickInsideModal(event) {
+  event.stopPropagation();
+}
+
+modalContentElem.addEventListener('click', onClickInsideModal);
+modalElem.addEventListener('click', closeModal);
+createEventCloseBtn.addEventListener('click', closeModal);
