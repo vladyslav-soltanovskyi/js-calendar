061462e93b36e5f3d7fa62d68291c6e4509470d1
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-10 20:08:14 +0100

    added scripts for timescale

--- scripts/calendar/timescale.js
+++ scripts/calendar/timescale.js
@@ -1,6 +1,23 @@
 import { createNumbersArray } from '../common/createNumbersArray.js';
 
+const generateSlots = (hours, text) => (
+  hours.reduce((template, i) => {
+    const isZero = i === 0;
+    return template + (
+      `<div class="time-slot">
+        <div class="time-slot__time">${!isZero ? i : ''} ${!isZero ? text : ''}</div>
+      </div>`
+    )
+  }, '')
+);
+
+const scaleTime = document.querySelector('.calendar__time-scale');
+
 export const renderTimescale = () => {
   // ф-ция должна генерировать разметку для боковой шкалы времени (24 часа)
   // полученную разметку вставьте на страницу с помощью innerHTML в .calendar__time-scale
+  const hoursInAM = createNumbersArray(0, 12);
+  const hoursInPM = createNumbersArray(1, 11);
+  const timeSlotsTemplate = generateSlots(hoursInAM, 'AM') + generateSlots(hoursInPM, 'PM');
+  scaleTime.innerHTML = timeSlotsTemplate;
 };
