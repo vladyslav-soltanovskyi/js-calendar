25a4a15cdd02f8b07e477177acdc03e2524d6af1
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-10 20:04:24 +0100

    added scripts for navigation

--- scripts/header/navigation.js
+++ scripts/header/navigation.js
@@ -11,11 +11,43 @@ const displayedMonthElem = document.querySelector(
 function renderCurrentMonth() {
   // отрисовать месяц, к которому относиться текущая неделя (getDisplayedMonth)
   // вставить в .navigation__displayed-month
+  displayedMonthElem.innerHTML = getDisplayedMonth(getItem('displayedWeekStart'));
 }
 
 const onChangeWeek = (event) => {
   // при переключении недели обновите displayedWeekStart в storage
   // и перерисуйте все необходимые элементы страницы (renderHeader, renderWeek, renderCurrentMonth)
+  const { target } = event;
+  const button = target.closest('[data-direction]');
+  const { direction } = button.dataset;
+
+  if (!direction) {
+    return;
+  }
+
+  let date = getStartOfWeek(getItem('displayedWeekStart'));
+  const dayOfMonth = date.getDate();
+  
+  switch(direction) {
+    case 'prev': {
+      date.setDate(dayOfMonth - 7);
+      break;
+    }
+    case 'next': {
+      date.setDate(dayOfMonth + 7);
+      break;
+    }
+    case 'today': {
+      date = getStartOfWeek(new Date());
+      break;
+    }
+  }
+  
+  setItem('displayedWeekStart', date);
+
+  renderWeek();
+  renderHeader();
+  renderCurrentMonth();
 };
 
 export const initNavigation = () => {
