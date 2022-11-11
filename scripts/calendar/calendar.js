54c3dee6c67259cd487e42434beffaa585aed1a7
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-11 14:10:03 +0100

    added scripts for calendar

--- scripts/calendar/calendar.js
+++ scripts/calendar/calendar.js
@@ -1,11 +1,30 @@
-// import { getItem } from '../common/storage.js';
-// import { generateWeekRange } from '../common/time.utils.js';
-// import { renderEvents } from '../events/events.js';
-// import { createNumbersArray } from '../common/createNumbersArray.js';
+import { getItem } from '../common/storage.js';
+import { generateWeekRange } from '../common/time.utils.js';
+import { renderEvents } from '../events/events.js';
+import { createNumbersArray } from '../common/createNumbersArray.js';
+
+const calendarWeek = document.querySelector('.calendar__week');
 
 const generateDay = () => {
   // функция должна сгенерировать и вернуть разметку дня в виде строки
   // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+  const hours = createNumbersArray(0, 23);
+  return hours.reduce(
+    (template, hour) => template + (
+      `<div class="calendar__time-slot" data-time="${hour}"></div>`
+    ), ''
+  )
+};
+
+const generateDays = () => {
+  // функция должна сгенерировать и вернуть разметку дня в виде строки
+  // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+  const days = generateWeekRange(getItem('displayedWeekStart'));
+  return days.reduce(
+    (template, day) => template + (
+      `<div class="calendar__day" data-day="${day.getDate()}">${generateDay()}</div>`
+    ), ''
+  )
 };
 
 export const renderWeek = () => {
@@ -14,4 +33,6 @@ export const renderWeek = () => {
   // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
   // каждый день должен содержать в дата атрибуте порядковый номер дня в месяце
   // после того, как отрисовали всю сетку для отображаемой недели, нужно отобразить события этой недели с помощью renderEvents
+  calendarWeek.innerHTML = generateDays();
+  renderEvents();
 };
