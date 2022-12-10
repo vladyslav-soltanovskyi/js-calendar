929dc7094790b425114755d4076ab7d0140e3e93
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-10 20:20:41 +0100

    finished project

--- scripts/calendar/timeLine.js
+++ scripts/calendar/timeLine.js
@@ -1,12 +1,23 @@
-export const createTimeLine = (offsetTop) => {
+const minute = 1000 * 60;
+let timerId;
+
+const createTimeLine = (offsetTop) => {
   const timeLine = document.createElement('div');
   timeLine.classList.add('time-line');
   timeLine.style.top = offsetTop + "px";
   return timeLine;
 }
 
+const removeTimerForTimeLine = () => {
+  const timeLine = document.querySelector('.time-line');
+  timeLine?.remove();
+  clearInterval(timerId);
+}
+
 export const renderTimeLine = () => {
+  removeTimerForTimeLine();
   const currentDate = new Date();
+  const delayForNextRender = minute - (currentDate.getSeconds() * 1000);
   
   const calendarTimeSlot = document.querySelector(
     `[data-day="${currentDate.getDate()}"] [data-time="${currentDate.getHours()}"]`
@@ -14,4 +25,6 @@ export const renderTimeLine = () => {
 
   const timeLine = createTimeLine(currentDate.getMinutes());
   calendarTimeSlot && calendarTimeSlot.append(timeLine);
+  
+  timerId = setInterval(renderTimeLine, delayForNextRender);
 };
